64f057c2bd5a88ce9d02821109e281a72338c0c5
Dylan Marriott <dylan@onyxleeds.co.uk>
2024-11-28 22:12:32 +0000

    SolanaSDK - Create Service Provider & Empty Test

---
+++ app-modules/solana-sdk/src/Providers/SolanaSDKServiceProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JadNetwork\SolanaSDK\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SolanaSDKServiceProvider extends ServiceProvider
+{
+    public function register(): void {}
+
+    public function boot(): void {}
+}
